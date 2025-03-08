71beb731d49cd07f1d86b5b6cdc3cbc1c822bd89
Bryanzinn81 <bryan.iagoo@gmail.com>
2025-03-07 21:57:11 -0300

    fazendo a parte de metodos da sintaxe - 5

--- DIO/bootcamp bradesco/est/sintaxe/tipos e variaveis/TiposVariaveis3.java
+++
@@ -1,9 +0,0 @@
-public class TiposVariaveis3 {
-    public static void main(String[] args) {
-        // Variaveis e Constantes
-
-        final double VALOR_DE_PI = 3.14; // com final não pode mudar o valor da variavel
-
-        System.out.print(VALOR_DE_PI);
-    }
-}
--- DIO/bootcamp bradesco/est/sintaxe/tipos e variaveis/TiposeVariaveis2.java
+++
@@ -1,7 +0,0 @@
-public class TiposeVariaveis2 {
-    public static void main(String[] args) {
-        short numeroCurto = 1;
-        int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal; // converção usando casting
-    }
-}
