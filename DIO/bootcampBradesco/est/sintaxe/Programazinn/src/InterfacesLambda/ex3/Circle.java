8c92bb0056107a81efc1cee8b1e352795cd8003d
Bryan Iago <160327635+Bryanzinn81@users.noreply.github.com>
2025-07-22 21:01:50 -0300

    Add files via upload

---
+++ DIO/bootcampBradesco/est/sintaxe/Programazinn/src/InterfacesLambda/ex3/Circle.java
@@ -0,0 +1,11 @@
+package InterfacesLambda.ex3;
+
+public record Circle(double radius) implements GeometricForm {
+	private static final double pi = 3.14;
+
+	@Override
+	public double getArea() {
+		return pi * (radius * radius);
+	}
+	
+}
---
+++ DIO/bootcampBradesco/est/sintaxe/Programazinn/src/InterfacesLambda/ex3/GeometricForm.java
@@ -0,0 +1,5 @@
+package InterfacesLambda.ex3;
+
+public interface GeometricForm {
+	double getArea();
+}
