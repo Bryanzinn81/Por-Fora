99bf5e806c38c2306ee013a152eee37fed5872e6
Bryan Iago <160327635+Bryanzinn81@users.noreply.github.com>
2025-07-29 21:39:50 -0300

    Add files via upload

---
+++ DIO/bootcampBradesco/ProjetoDio - IphoneUML/Dispositive.java
@@ -0,0 +1,7 @@
+package IphoneUML;
+
+interface Dispositive {
+	public void startVoicemail();
+	public void acceptCall() ;
+	public void call(String number) ;
+}
---
+++ DIO/bootcampBradesco/ProjetoDio - IphoneUML/Music.java
@@ -0,0 +1,7 @@
+package IphoneUML;
+
+interface Music{
+	public void play();
+	public void pause();
+	public void selectMusic(String music) ;
+}
